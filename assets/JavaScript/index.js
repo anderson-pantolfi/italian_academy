dd6f03bbdc510e6bd4a202f199c7753db28b2c1c
anderson-pantolfi-moraes <andpantmoraes30@gmail.com>
2024-07-17 09:55:35 -0300

    correção do bug no efeito TyperWriter no aquivo index e adicionado efeito slidershow na div depoimentos

---
+++ assets/JavaScript/index.js
@@ -0,0 +1,41 @@
+var swiper = new Swiper(".swiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 2,
+    loop: true,
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
+
+
+  function effectTyperWriter(caracter, loop = true) {
+    const textArray = caracter.innerHTML.split("");
+    caracter.innerHTML = "";
+  
+    textArray.forEach((letra, i) => {
+      setTimeout(() => (caracter.innerHTML += letra), 200 * i);
+    });
+  
+    if (loop) {
+      setTimeout(() => effectTyperWriter(caracter, loop), 200 * textArray.length);
+    }
+  }
+  
+  const titulo = document.getElementById("typeWriter");
+  effectTyperWriter(titulo);
+
+
