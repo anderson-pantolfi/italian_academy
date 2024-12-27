bc194a0050ab253c898b53c47ceae8c79e389e2c
anderson-pantolfi-moraes <andpantmoraes30@gmail.com>
2024-12-27 18:40:48 -0300

    finalização teste 2

--- assets/Typescript/teste-questao.ts
+++ assets/Typescript/teste-questao.ts
@@ -240,3 +240,245 @@ const teste1: typequestion[] = [
         answer: 0,
     }
 ]
+
+const teste2:typequestion[] = [
+ {
+    question: "Quanti libri leggi al mese?",
+    option: [
+        "Li leggo due.",
+        "Leggo due.",
+        "Ne leggo due",
+    ],
+    answer: 2,
+ },
+
+ {
+    question: "Buongiorno, signora Rossi, cosa desidera oggi?",
+    option: [
+        "Buongiorno. Vorrei due etti di prosciutto crudo e mezzo chilo di parmigiano reggiano.",
+        "Buongiorno. No, basta così",
+        "Grazie. Quant'è?"
+    ],
+    answer: 0,
+ },
+
+ {
+    question: "Abiti da solo?",
+    option: [
+        "No, abito con mie sorelle",
+        "No, abito con mia sorella.",
+        "No, abito con la mia sorella",
+    ],
+    answer: 2,
+ },
+
+ {
+    question: "Domenica scorsa cosa hai fatto?",
+    option: [
+        "Mi ho alzata tardi e poi ho fatto colazione.",
+        "Mi sono alzata tardi e poi ho fatto colazione.",
+        "Ho alzata tardi e poi ho fatto colazione.",
+    ],
+    answer: 1,
+ },
+
+ {
+    question:"Quant'è?",
+    option: [
+        "In totale sono 25 euro.",
+        "Viene 25 euro.",
+        "Vengono 25 euro",
+    ],
+    answer: 2,
+ },
+
+ {
+    question: "Com'è il clima oggi nella tua città?",
+    option: [
+        "Fa caldo, ci sono 33 gradi ed è un po' nuvoloso.",
+        "Fa caldo, ci sono 33 gradi e un vento gelido.",
+        "Fa bel tempo: è nuvoloso e piove.",
+    ],
+    answer: 0,
+ },
+
+ {
+    question: "Siete andati al bar tu e Paolo ieri sera?",
+    option: [
+        "Sì e abbiamo preso qualche birra insieme.",
+        "Sì, e abbiamo prenduto qualche birra insieme.",
+        "Sì e siamo presi qualche birra insieme.",
+    ],
+    answer: 0,
+ },
+
+ {
+    question: "Sai che oggi è il mio compleanno?",
+    option: [
+        "Davvero? Buon anno!",
+        "Davvero? Congratulazioni!",
+        "Davvero? Tanti auguri!,",
+    ],
+    answer: 2,
+ },
+
+ {
+    question:"Dov'è il libro di Maria?",
+    option: [
+        "Suo libro è sul tavolo.",
+        "La sua libro è sul tavolo.",
+        "Il suo libro è sul tavolo.",
+    ],
+    answer: 2,
+ },
+
+ {
+    question: "Quando fai i compiti per casa?",
+    option: [
+        "Li faccio la sera.",
+        "Lo faccio la sera.",
+        "Faccio la sera.",
+    ],
+    answer: 0,
+ },
+
+ {
+    question:"Benissimo. Qualcos'altro?",
+    option: [
+        "Sì, quant'è?",
+        "Ne vorrei due etti, per favore.",
+        "No, basta così. Quant'è?",
+    ],
+    answer: 2,
+ },
+
+ {
+    question:"Sai, domani devo fare un esame.",
+    option: [
+        "In bocca al lupo!",
+        "Complimenti!",
+        "Buon viaggio!",
+    ],
+    answer: 0,
+ },
+
+ {
+    question: "Dov'è il Duomo?",
+    option: [
+        "Accanto alla Galleria Vittorio Emanuele.",
+        'Vicino della Galleria Vittorio Emanuele.',
+        "Dietro della Galleria Vittorio Emanuele",
+    ],
+    answer: 0,
+ },
+
+ {
+    question:"Esci con noi stasera?",
+    option: [
+        "Non posso, domani mi sveglio presto per andare a lavorare.",
+        "Non posso, domani sveglio presto per andare a lavorare.",
+        "Non posso, domani mi sveglio tardi per andare a lavorare.",
+    ],
+    answer: 0,
+ },
+
+ {
+    question: "Ordiniamo una bottiglia di vino rosso?",
+    option: [
+        "No! Ma che dici? Con il pesce beve il vino bianco.",
+        "No! Ma che dici? Con il pesce si beve il vino bianco.",
+        "No! Ma che dici? Con il pesce si bevono il vino bianco.",
+    ],
+    answer: 1,
+ },
+
+ {
+    question:"Quando siete andati in montagna tu e tua moglie?",
+    option: [
+        "Ci siamo andati sabato scorso.",
+        "Ci andiamo sabato scorso.",
+        "Ci siamo andati sabato prossimo.",
+    ],
+    answer: 0,
+ },
+
+ {
+    question:"Cosa ha fatto Paola ieri sera?",
+    option: [
+        "È rimasto a casa a guardare la tv.",
+        "È rimanuta a casa a guardare la tv.",
+        "È rimasta a casa a guardare la tv.",
+    ],
+    answer: 2,
+ },
+
+ {
+    question: "Come si chiamano i tuoi genitori?",
+    option: [
+        "Mio padre si chiama Carlo e mia madre si chiama Lucia.",
+        "Il mio padre si chiama Carlo e la mia madre si chiama Lucia.",
+        "Mio babbo si chiama Carlo e mia mamma si chiama Lucia.",
+    ],
+    answer: 0,
+ },
+
+ {
+    question:"Buongiorno, vorrei:",
+    option: [
+        "del Parmigiano Reggiano.",
+        "dello Parmigiano Reggiano.",
+        "dell' Parmigiano Reggiano.",
+    ],
+    answer: 0,
+ },
+
+ {
+    question:"Di solito Giovanni si sveglia alle sette di mattina ma rimane a letto almeno venti minuti prima di alzarsi. Poi si lava, si veste e fa colazione. Esce di casa verso le 8 per andare in ufficio. Lui fa l'avvocato e lavora dal lunedì al venerdì. Quando torna a casa dall'ufficio si riposa un po' e poi va a giocare a calcetto con gli amici oppure ascolta un po' di musica. Cena a casa alle otto e mezza. In genere va a letto a mezzanotte. Ieri, sabato, come al solito Giovanni non ha lavorato. Si è alzato più tardi, verso le 9, ha fatto la doccia e ha letto il giornale. Poi ha guardato la tv e ha pranzato con sua moglie e sua figlia. Il pomeriggio lui e la sua famiglia sono andati al cinema a vedere l'ultimo film di Paolo Virzì.",
+    option: [
+        "Giovanni acorda às sete e trabalha a semana toda, de segunda a sábado.",
+        "Giovanni descansa depois do trabalho, assiste TV ou vai ao cinema.",
+        "De segunda a sexta o Giovanni acorda às sete e vai dormir à meia-noite",
+    ],
+    answer: 0,
+ },
+
+ {
+    question: "Ho visitato l’Italia.",
+    option: [
+        "anno scorso",
+        "fa un anno",
+        "l'anno scorso",
+    ],
+    answer: 2,
+ },
+
+ {
+    question:"Sabato scorso io e mia moglie",
+    option: [
+        "ci siamo alzati tardi.",
+        "ci siamo alzato tardi.",
+        "ci abbiamo alzato tard",
+    ],
+    answer: 0,
+ },
+
+ {
+    question:"Scegli l'opzione corretta:",
+    option: [
+        "Miei fratelli abitano a Verona.",
+        "Mia sorelle abitano a Verona.",
+        "I miei fratelli abitano a Verona,",
+    ],
+    answer: 2,
+ },
+
+ {
+    question:"Scegli l'opzione corretta:",
+    option: [
+        '"Cosa fai a Ferragosto?" "Sto a casa con la mia famiglia, mangiamo il panettone, apriamo i regali e beviamo lo spumante.!"',
+        '"Cosa fai a Natale?" "Sto a casa con la mia famiglia, mangiamo il panettone, apriamo i regali e beviamo lo spumante.!"',
+        '"Cosa fai a Carnevale?" "Sto a casa con la mia famiglia, mangiamo il panettone, apriamo i regali e beviamo lo spumante.!"',
+    ],
+    answer: 1,
+ },
+]
